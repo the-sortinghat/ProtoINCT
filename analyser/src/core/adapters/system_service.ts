901fca0ade2f3fd343c06fc1da856c52ebd9d825
João Daniel <jotaf.daniel@gmail.com>
2021-05-19 10:50:20 -0300

    fix misimported System in service interface
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- analyser/src/core/adapters/system_service.ts
+++ analyser/src/core/adapters/system_service.ts
@@ -1,4 +1,4 @@
-import { Service } from "./entities/service";
+import { System } from "../entities/system";
 
 export interface SystemServiceInterface {
   createSystem: (system: System) => Promise<void>;
