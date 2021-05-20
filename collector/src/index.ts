6a50bcb1ee1c8153f6f732880b410ef548df521d
Bruno Romano <brunopromano@gmail.com>
2021-05-20 21:43:22 +0000

    WIP: create register route and test js-yaml
    Co-authored-by: Bruno Romano <brunopromano@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 
@@ -8,6 +11,21 @@ app.use(express.json());
 
 app.get('/', (_, res) => res.status(200).json({ message: 'Hello' }));
 
+app.post('/register', (req, res) => {
+  const { repoURL } = req.body;
+  const rgx = /(?:https?:\/\/)?(?:www\.)?github\.com\/(.+)\/(.+)\/?/;
+  const match = repoURL.match(rgx);
+
+  const [_, userOrRepoName, repoName] = match;
+
+  return res.json({ userOrRepoName, repoName });
+});
+
 app.listen(port, () => {
   console.log('Collector is running!');
+  const filePath = path.resolve(__dirname, '..', 'test.yaml');
+  const rawFile = fs.readFileSync(filePath).toString();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dockerCompose = yaml.load(rawFile) as any;
+  console.log(dockerCompose.services.broker.image);
 });
