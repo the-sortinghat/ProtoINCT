3c6ce0946758cf93451b2bfe13d45c9fa519f584
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-05-19 21:20:38 -0300

    finish scaffold app
    add express, nats client for node and some scripts
    
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Bruno Romano <brunopromano@gmail.com>

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello');
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => res.status(200).json({ message: 'Hello' }));
+
+app.listen(port, () => {
+  console.log('Collector is running!');
+});
