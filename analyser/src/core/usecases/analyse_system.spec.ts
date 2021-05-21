692172e57f2436b587d527e49415b3cdfe5e6082
João Daniel <jotaf.daniel@gmail.com>
2021-05-21 11:23:37 -0300

    test corner case of mixed usage of database
    mixed: both namespaced and free
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- analyser/src/core/usecases/analyse_system.spec.ts
+++ analyser/src/core/usecases/analyse_system.spec.ts
@@ -1,4 +1,6 @@
 import { MessageBrokerClientInterface } from "../adapters/message_broker_client";
+import { Database } from "../entities/database";
+import { Service } from "../entities/service";
 import { System } from "../entities/system";
 import { AnalyseSystem } from "./analyse_system";
 
@@ -21,6 +23,46 @@ describe("AnalyseSystem", () => {
       systemName,
       payload: {
         sharedDatabase: [],
+        databasePerService: [],
+      },
+    };
+
+    expect(mockBroker.publish).toHaveBeenCalledWith(
+      "system_analysed",
+      JSON.stringify(event)
+    );
+  });
+
+  it("identifies as sharedDatabase when a namespaced and a free services are bound to the same database", () => {
+    const systemName = "test";
+    const system = new System(systemName);
+
+    const db = new Database("document", "MongoDB", 1);
+    const svc1 = new Service("foo", 1);
+    const svc2 = new Service("bar", 2);
+
+    db.addService(svc1);
+    db.addService(svc2, "baz");
+
+    system.addDatabase(db);
+
+    target.run(system);
+
+    const sharedDatabase = [
+      {
+        databaseID: 1,
+        services: [
+          { name: "bar", id: 2 },
+          { name: "foo", id: 1 },
+        ],
+      },
+    ];
+
+    const event = {
+      type: "system analysed",
+      systemName,
+      payload: {
+        sharedDatabase,
         databasePerService: [],
       },
     };
