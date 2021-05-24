8d1a6a33875fab08b8645d11e5ef74a797a8204d
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-05-24 20:02:45 -0300

    WIP: fetch docker-compose file from github
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Bruno Romano <brunopromano@gmail.com>

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import yaml from 'js-yaml';
-import fs from 'fs';
-import path from 'path';
+import axios from 'axios';
 
 const port = process.env.PORT || 3000;
 
@@ -11,21 +10,22 @@ app.use(express.json());
 
 app.get('/', (_, res) => res.status(200).json({ message: 'Hello' }));
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { repoURL } = req.body;
   const rgx = /(?:https?:\/\/)?(?:www\.)?github\.com\/(.+)\/(.+)\/?/;
   const match = repoURL.match(rgx);
+  const userOrOrgName = match[1];
+  const repoName = match[2];
 
-  const [_, userOrRepoName, repoName] = match;
+  const url = `https://raw.githubusercontent.com/${userOrOrgName}/${repoName}/main/docker-compose.yaml`;
+  const response = await axios.get(url);
 
-  return res.json({ userOrRepoName, repoName });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dockerCompose = yaml.load(response.data) as any;
+
+  return res.json({ userOrOrgName, repoName, brokerImage: dockerCompose.services.broker.image });
 });
 
 app.listen(port, () => {
   console.log('Collector is running!');
-  const filePath = path.resolve(__dirname, '..', 'test.yaml');
-  const rawFile = fs.readFileSync(filePath).toString();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const dockerCompose = yaml.load(rawFile) as any;
-  console.log(dockerCompose.services.broker.image);
 });
