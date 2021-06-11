3798624ab4ad8a2ed252604681e2c34c5aa7ee71
João Daniel <jotaf.daniel@gmail.com>
2021-06-10 22:51:39 -0300

    reshape edge
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- collector/src/entities/edge.ts
+++ collector/src/entities/edge.ts
@@ -1,5 +1,26 @@
-import { Node } from './node';
+type Vertex = any;
 
 export class Edge {
-  constructor(public readonly node: Node, public readonly payload: any = null) {}
+  private static nextID = 0;
+  private _id: number;
+
+  constructor(private readonly v: Vertex, private readonly w: Vertex, public readonly payload: any = null) {
+    this._id = Edge.nextID++;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  either(): Vertex {
+    return this.v;
+  }
+
+  other(v: Vertex): Vertex {
+    return v.id === this.v.id ? this.w : this.v;
+  }
+
+  compareTo(that: Edge): number {
+    return this.id - that.id;
+  }
 }
