8725b2bc7b65e0628fbcc857cd844bfcd359b68c
João Daniel <jotaf.daniel@gmail.com>
2021-06-21 14:21:47 -0300

    add a first version of preconditions page
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ frontend/components/Preconditions.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>
+      This work is a prototype, it has a narrow scope of implementation yet. The
+      conditions under which this tool works are the following:
+    </p>
+
+    <ul class="list-disc list-inside m-2">
+      <li>
+        Only repositories hosted at
+        <a target="_blank" href="github.com">GitHub</a>
+      </li>
+
+      <li>
+        Projects must be configured to use
+        <a target="_blank" href="https://docs.docker.com/compose/">
+          Docker Compose
+        </a>
+        for development
+      </li>
+
+      <li>
+        The <code>docker-compose.yaml</code> must be named exactly like that and
+        must be placed at the root of the repository
+      </li>
+
+      <li>
+        As the prototype focus on <strong>Database per Service</strong>, there
+        is a list of supported DBMS:
+
+        <ul class="ml-8 list-disc list-inside">
+          <li>MongoDB</li>
+
+          <li>PostgreSQL</li>
+
+          <li>MySQL</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ frontend/pages/preconditions.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto p-2 h-screen">
+    <Preconditions />
+  </div>
+</template>
