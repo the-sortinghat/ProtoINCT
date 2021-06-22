21fe1cf5647526b57fa744aa2332bb1954c8480a
João Daniel <jotaf.daniel@gmail.com>
2021-06-22 10:17:42 -0300

    restart from scratch
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- analyser/src/core/entities/types.d.ts
+++
@@ -1,5 +0,0 @@
-export type ID = string | number;
-export type MaybeNamespace = string | null;
-export type Namespace = string;
-export type Message = string | Buffer;
-export type MessageCallback = (msg: any) => void;
